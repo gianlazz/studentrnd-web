5e4c98cff1dc7819a0e0dad879de33c3f0e2cd50
tangmi <mimimimami@gmail.com>
2012-09-05 20:01:46 -0700

    Added summer/not summer hours case

--- index.php
+++ index.php
@@ -31,6 +31,8 @@ switch($_SERVER['REQUEST_URI']) {
 
 $tang = "no-flip"; //fixes CSS4 bug 
 
+$openhours = false; //is it summer time?
+
 //information about StudentRND, displayed on the site
 $addressname = "StudentRND";
 $address1 = "1405 132nd Ave NE, Suite 3";
@@ -38,6 +40,11 @@ $address2 = "Bellevue, WA 98005";
 $hours = "Mon-Fri 10-7p, Sat 12-7p";
 $phone = "Tel: (425) 780-7901";
 
+
+if(!$openhours) {
+	// unset($hours);
+	$hours = "Not open outside summer.";
+}
 
 set_include_path("./pages/");
 include "header.php";
