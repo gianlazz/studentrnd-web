74ce83b344b490eb4abaae43c412faba8b53ffe4
tangmi <mimimimami@gmail.com>
2012-09-05 19:42:36 -0700

    Added CodeDay past events link/section

--- pages/about.php
+++ pages/about.php
@@ -5,10 +5,11 @@
 			<h2>Why does StudentRND matter?</h2>
 			<p>Technology is so prevalent nowadays&#8211;and yet, students are not really learning about the fields of science, technology, engineering, and mathematics. StudentRND is working to inspire students&#8211;through our enriching, hands-on workspace experience, through educational visits to science labs, and through a sense of community.</p>
 			<h2>Projects</h2>
-			<p></p>
+			<h3>Student Created</h3>
 			<ul id="projects">
 				<li><span class="name">Fixing Adam's CSS</span> <span class="info">Michael Tang, 2012</span></li>
-				<li><span class="name"><a href="http://store.studentrnd.org/products/plasma-speaker">Plasma Speaker</a></span> <span class="info">Marshall Meng &#38; Matt Chapman 2011</span></li>
+				<li><span class="name"><a href="http://store.studentrnd.org/products/plasma-speaker">Plasma Speaker</a></span>
+					<span class="info">Marshall Meng &#38; Matt Chapman 2011</span></li>
 				<li><span class="name"><a href="/about/robotic-pipe-organ">Robotic Pipe Organ</a></span> <span class="info">David Tyler 2011</span></li>
 				<li><span class="name">RepRap</span> <span class="info">Eric Yu 2011</span></li>
 				<li><span class="name">CNC Router</span> <span class="info">Nolan Miller 2011</span></li>
@@ -17,6 +18,9 @@
 				<li><span class="name"><a href="http://wiki.studentrnd.org/3D_Printer">Makerbot Cupcake</a></span> <span class="info">2010</span></li>
 				<li><span class="name">Hoverboard</span> <span class="info">Charles Niu 2010</span></li>
 			</ul>
+			<h3>CodeDay</h3>
+			<p>StudentRND hosts a 24-hour programming competition every few months called CodeDay.</p>
+			<p><a href="http://codeday.org/pastevents.php" title="Past CodeDay Events" target="_BLANK">Check out past CodeDay events</a>.</p>
 			<h2>Want to know more about us?</h2>
 			<p>Check out our <a href="http://wiki.studentrnd.org" class="broken_link">online resources</a>.</p>
 			<p>Look at our <a href="http://blog.studentrnd.org">blog</a>.</p>
