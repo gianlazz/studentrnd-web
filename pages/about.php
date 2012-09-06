03c6368739fdc270034f90cef0486bba5e9a861b
Your Name <adamryman@gmail.com>
2012-09-05 19:19:13 -0700

    Changed about's top sentence and added to project list

--- pages/about.php
+++ pages/about.php
@@ -1,26 +1,26 @@
 <div class="row">
 	<div class="span12">
-		<h1>StudentRND inspires students to learn more about science and technology by building cool stuff. Adam is expected to write one or two more sentences pitching StudentRND.</h1>
+		<h1>StudentRND is a community of students that love science and technology. Though StudentRND resources, events, and programs students build amazing things.</h1>
 		<div class="box">
 			<h2>Why does StudentRND matter?</h2>
 			<p>Technology is so prevalent nowadays&#8211;and yet, students are not really learning about the fields of science, technology, engineering, and mathematics. StudentRND is working to inspire students&#8211;through our enriching, hands-on workspace experience, through educational visits to science labs, and through a sense of community.</p>
 			<h2>Projects</h2>
-			<p>here are some projects taht ahave somei faoisdoife, and adam is expected to credit people and their projects</p>
+			<p></p>
 			<ul id="projects">
 				<li><span class="name">Fixing Adam's CSS</span> <span class="info">Michael Tang, 2012</span></li>
-				<li><span class="name">Plasma Speaker</span> <span class="info">2011</span></li>
-				<li><span class="name"><a href="/about/robotic-pipe-organ">Robotic Pipe Organ</a></span> <span class="info">2011</span></li>
-				<li><span class="name">RepRap</span> <span class="info">2011</span></li>
-				<li><span class="name">CNC Router</span> <span class="info">2011</span></li>
-				<li><span class="name">Statspad Baller</span> <span class="info">2011</span></li>
+				<li><span class="name"><a href="/about/robotic-pipe-organ">Robotic Pipe Organ</a></span> <span class="info">David Tyler 2011</span></li>
+				<li><span class="name"><a href="http://store.studentrnd.org/products/plasma-speaker">Plasma Speaker</a></span> <span class="info">Marshall Meng &#38; Matt Chapman 2011</span></li>
+				<li><span class="name"><a href="/about/robotic-pipe-organ">Robotic Pipe Organ</a></span> <span class="info">David Tyler 2011</span></li>
+				<li><span class="name">RepRap</span> <span class="info">Eric Yu 2011</span></li>
+				<li><span class="name">CNC Router</span> <span class="info">Nolan Miller 2011</span></li>
+				<li><span class="name">Statspad Baller</span> <span class="info">Adam Stephenson 2011</span></li>
 				<li><span class="name">Surface Computer</span> <span class="info">2010</span></li>
 				<li><span class="name"><a href="http://wiki.studentrnd.org/3D_Printer">Makerbot Cupcake</a></span> <span class="info">2010</span></li>
-				<li><span class="name">Hoverboard</span> <span class="info">2010</span></li>
+				<li><span class="name">Hoverboard</span> <span class="info">Charles Niu 2010</span></li>
 			</ul>
 			<h2>Want to know more about us?</h2>
-			<p>Find out <a href="http://studentrnd.org/leadership" target="_blank" class="broken_link">who&#8217;s in charge</a>.</p>
-			<p>Want to <a href="http://studentrnd.org/about/private-groups/">rent the space</a> for a private group? --- put in "Get Involved"</p>
-			<p>Check out our <a href="http://studentrnd.org/documents" class="broken_link">online resources</a>.</p>
+			<p>Check out our <a href="http://wiki.studentrnd.org" class="broken_link">online resources</a>.</p>
+			<p>Look at our <a href="http://blog.studentrnd.org">blog</a>.</p>
 		</div>
 	</div>
 </div>
