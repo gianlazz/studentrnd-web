1058ff9ab6ff2f421ccef16c03f17ae7ac7f624b
tangmi <mimimimami@gmail.com>
2012-09-06 00:15:27 -0700

    Wrapped page content

--- pages/issomebodythere.php
+++ pages/issomebodythere.php
@@ -1,14 +1,20 @@
-<?php
-if($open == true) {
-	echo <<<TANG
-	<h1>Yes, somebody is here</h1>
-	<p>You'll be able to get in.</p>
+<div class="row">
+	<div class="span12">
+		<div class="box">
+			<?php
+			if($open == true) {
+				echo <<<TANG
+				<h1>Yes, somebody is here</h1>
+				<p>You'll be able to get in.</p>
 TANG;
-} else {
-	echo <<<TANG
-	<h1>No, nobody is here</h1>
-	<p>...but, you can call us if you need after hours access.</p>
-	<p>$phone;</p>
+			} else {
+				echo <<<TANG
+				<h1>No, nobody is here</h1>
+				<p>...but, you can call us if you need after hours access.</p>
+				<p>$phone;</p>
 TANG;
-}
-?>
+			}
+			?>
+		</div>
+	</div>
+</div>
